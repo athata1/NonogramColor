475e3889d411d55c996716ba954a1a36e915a022
Akhil Thata <akhil.thata@gmail.com>
2022-11-23 23:08:49 -0500

    Implemented and tested NonogramProbabilityThread

---
+++ src/NonogramProbabilityThread.java
@@ -0,0 +1,108 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NonogramProbabilityThread implements Runnable{
+
+
+    private char[] arr;
+    private RuleData rd;
+    private HashMap<Character, int[]> prob;
+    private HashMap<Character, Color> map;
+    private int total;
+    public NonogramProbabilityThread(RuleData rd, char[] arr, HashMap<Character, Color> map) {
+        this.rd = rd;
+        this.arr = arr;
+        this.map = map;
+        this.prob = new HashMap<>();
+        for (Map.Entry<Character, Color> entry: map.entrySet()) {
+            prob.put(entry.getKey(), new int[arr.length]);
+        }
+        this.total = 0;
+    }
+
+    public void run() {
+        if (rd.getNumRule()[0] == 0) {
+            return;
+        }
+        findProb(rd.getStartIndex(), rd.getStart(), new ArrayList<Integer>());
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != '_')
+                continue;
+
+            int countZeroes = 0;
+            for (Map.Entry<Character, Color> entry: map.entrySet()) {
+                int counter = prob.get(entry.getKey())[i];
+                if (counter == 0) {
+                    countZeroes++;
+                }
+                else if (counter == total) {
+                    arr[i] = entry.getKey();
+                }
+            }
+            if (countZeroes == prob.size()) {
+                arr[i] = 'X';
+            }
+        }
+
+    }
+
+    public void findProb(int ruleIndex, int arrIndex, ArrayList<Integer> indexes) {
+
+        int[] numRule = rd.getNumRule();
+        char[] colorRule = rd.getColorRule();
+
+        if (ruleIndex == numRule.length) {
+            char[] temp = new char[arr.length];
+            Arrays.fill(temp, 'X');
+            for (int i = 0; i < numRule.length; i++) {
+                for (int j = 0; j < numRule[i]; j++) {
+                    temp[j + indexes.get(i)] = colorRule[i];
+                }
+            }
+            System.out.println(Arrays.toString(temp));
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == '_')
+                    continue;
+                else if (arr[i] == 'X' && temp[i] == 'X')
+                    continue;
+                else if (arr[i] != 'X' && temp[i] != 'X' && arr[i] == temp[i])
+                    continue;
+                else
+                    break;
+            }
+            total++;
+            for (int i = 0; i < temp.length; i++) {
+                if (temp[i] == 'X')
+                    continue;
+
+                prob.get(temp[i])[i]++;
+            }
+            return;
+        }
+
+        //Get total length of string
+        int length = numRule[ruleIndex];
+        for (int i = ruleIndex + 1; i < numRule.length; i++) {
+            length += numRule[i];
+            if (colorRule[i] == colorRule[i - 1])
+                length += 1;
+        }
+
+        for (int i = arrIndex; i < arr.length - length + 1; i++) {
+            indexes.add(i);
+            int nextStart = i + numRule[ruleIndex];
+            if (ruleIndex + 1 < numRule.length && colorRule[ruleIndex] == colorRule[ruleIndex + 1]) {
+                nextStart++;
+            }
+            findProb(ruleIndex + 1, nextStart, indexes);
+            indexes.remove(indexes.size() - 1);
+        }
+    }
+
+    public char[] getArr() {
+        return arr;
+    }
+}
--- src/TheoryTest.java
+++ src/TheoryTest.java
@@ -1,6 +1,23 @@
+import java.awt.*;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class TheoryTest {
-    public static void main(String[] args) {
-        int[] ruleNums = new int[3];
-        char[] ruleColors = new char[3];
+    public static void main(String[] args) throws Exception{
+        String[] rules = {"1", "2", "3", "A", "B", "B"};
+        HashMap<Character, Color> map = new HashMap<Character, Color>();
+        map.put('A', new Color(0,0,0));
+        map.put('B', new Color(2,4,6));
+        map.put('C', new Color(123, 234, 145));
+        char[] arr = new char[8];
+        Arrays.fill(arr, '_');
+        RuleData rd = new RuleData(rules, arr.length);
+        NonogramProbabilityThread th = new NonogramProbabilityThread(rd, arr, map);
+        Thread t = new Thread(th);
+        t.start();
+
+        t.join();
+        System.out.println(Arrays.toString(th.getArr()));
+
     }
 }
