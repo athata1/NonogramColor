f9435ada2e9e031d2e7e3df1b05ea02889afb194
Akhil Thata <akhil.thata@gmail.com>
2022-11-26 22:39:47 -0500

    Updated Main and TheoryTest

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        NonogramColorSolver n = new NonogramColorSolver("Nonogram1");
 
+        /*while (true) {
+            long a = System.currentTimeMillis();
+            NonogramColorSolver n = new NonogramColorSolver("Nonogram3");
+            n.solveNonogram();
+            System.out.println(System.currentTimeMillis() - a);
+        }*/
+
+        NonogramColorSolver n = new NonogramColorSolver("Nonogram2");
+        n.solveNonogram();
+        n.printCurrentBoard();
     }
 }
--- src/TheoryTest.java
+++ src/TheoryTest.java
@@ -4,14 +4,14 @@ import java.util.HashMap;
 
 public class TheoryTest {
     public static void main(String[] args) throws Exception{
-        String[] rules = {"1", "2", "3", "A", "B", "C"};
+        String[] rules = "1 1 1 1 1 2 1 5 1 2 1 A C A A A E J E J E A".split(" ");
         HashMap<Character, Color> map = new HashMap<Character, Color>();
         map.put('A', new Color(0,0,0));
-        map.put('B', new Color(2,4,6));
+        map.put('E', new Color(2,4,6));
         map.put('C', new Color(123, 234, 145));
-        char[] arr = new char[7];
+        map.put('J', null);
+        char[] arr = new char[20];
         Arrays.fill(arr, '_');
-        arr[3] = 'X';
         RuleData rd = new RuleData(rules, arr.length);
         NonogramProbabilityThread th = new NonogramProbabilityThread(rd, arr, map);
         Thread t = new Thread(th);
